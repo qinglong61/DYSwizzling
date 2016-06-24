dbdff9f96ebe34daab447665323358860303ea31
duanyu <qinglong61@163.com>
2016-06-24 18:06:07 +0800

    Initial Commit

---
+++ DYSwizzling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYSwizzling
+//
+//  Created by duanqinglun on 16/6/24.
+//  Copyright © 2016年 duan.yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYSwizzling/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DYSwizzling
+//
+//  Created by duanqinglun on 16/6/24.
+//  Copyright © 2016年 duan.yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
